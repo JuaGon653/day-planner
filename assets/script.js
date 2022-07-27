d21a9a5f78b216c0b6204e70485a234f489c4150
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-07-27 17:27:24 -0500

    added comments

--- assets/script.js
+++ assets/script.js
@@ -14,6 +14,7 @@ var throughTF = 8;
 var afterTwelve = 0;
 // loops to create the 9 time blocks
 for(var i = 8; i <= 16; i++){
+    // row element of the table
     var tableRowEl = $('<tr></tr>');
     var amORpm = 'AM';
     var temp = 0;
@@ -25,7 +26,9 @@ for(var i = 8; i <= 16; i++){
         afterTwelve++;
     }
     tableRowEl.attr("class", "row");
+    // first column display the time
     tableRowEl.append('<td class="col-2 custom-col">' + (i+1) + amORpm + '</td>');
+    // checks if time blocks are in the past, future, or present and changes their color appearance based off of time
     if(throughTF+1 < moment().format("H")){
         tableRowEl.append('<td class="col-8 custom-col past"><textarea class="row' + throughTF + '"></textarea></td>');
     } else if (throughTF+1 > moment().format("H")){
@@ -33,15 +36,21 @@ for(var i = 8; i <= 16; i++){
     } else {
         tableRowEl.append('<td class="col-8 custom-col present"><textarea class="row' + throughTF + '"></textarea></td>');
     }
+    // third column then contains a save button
     tableRowEl.append('<td class="col-2 custom-col"><img class="saveBtn" id="row' + throughTF + '" src="./assets/images/save-image.png"></td>');
     tableBodyEl.append(tableRowEl);
+    // if it's the afternoon, 'i' is set back to the original 1-24 time
     if(amORpm === 'PM'){
         i = temp;
     }
     throughTF++;
 }
+// saves the save buttons and textareas after they have all been created
 saveBtnEl = $('.saveBtn');
 textInput = $('textarea');
+
+// if it is a new day the older events are deleted
+// if not a new day, all events are pulled from the local storage
 var prevDay = localStorage.getItem('prevDay');
 if(prevDay < moment().format("DDD")){
     textArray = [];
@@ -52,17 +61,20 @@ if(prevDay < moment().format("DDD")){
         textArray = JSON.parse(localStorage.getItem("textArray"));
     }
 }
+// displays textarea inputs
 for(var i = 0; i <= textInput.length; i++){
     textInput.eq(i).val(textArray[i]); 
 }
 
-
+// save button saves the textarea input of the row you pressed the save button
 saveBtnEl.on('click', function(event) {
-    event.stopPropagation();
     event.preventDefault();
+    // gets the save button element's id attribute that you pressed
     var id = this.id;
     var tempArray = [];
-    for(var i = 0; i < 24; i++){
+    // goes through all the time blocks to see which one has the same class name as the pressed element's id
+    // or if the input was already in the saved array in order to save the event
+    for(var i = 8; i <= 16; i++){
         if (id == textInput.eq(i).attr('class') || (textInput.eq(i).val() == textArray[i])){
             tempArray.push(textInput.eq(i).val());
         } else {
@@ -70,6 +82,7 @@ saveBtnEl.on('click', function(event) {
         }
     }
     textArray = tempArray;
+    // sets the textArray variable in the local storage to the saved inputs from the tempArray
     localStorage.setItem("textArray", JSON.stringify(textArray));
     localStorage.setItem("prevDay", moment().format("DDD"));
 })
